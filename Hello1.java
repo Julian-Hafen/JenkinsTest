35c92eb16ed666511be6da64cf02856d7b786a0a
Julian <julian.hafen@pks.de>
2020-08-18 10:06:51 +0200

    Habe das Hallo1 Welte Programm bei der Ausgabe verändert

--- Hello1.java
+++ Hello1.java
@@ -3,7 +3,7 @@ public class Hello1{
 	public static void main(String[] args){
 
 		for(int i=1;i<10;i++){
-		System.out.println("Hello Worold"+i);
+		System.out.println("Hallo Welt, kannst du mich hoeren?"+i);
 		}
 
 	}
