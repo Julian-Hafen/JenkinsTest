3e17e4d9486188104aa8144a68b8b643cb2bb7f6
Julian <julian.hafen@pks.de>
2020-08-19 14:29:59 +0200

    Ausgabe 15 mal

--- Hello1.java
+++ Hello1.java
@@ -2,7 +2,7 @@ public class Hello1{
 
 	public static void main(String[] args){
 
-		for(int i=1;i<10;i++){
+		for(int i=1;i<15;i++){
 		System.out.println("Hallo Welt, kannst du mich hoeren?? "+i);
 		}
 
